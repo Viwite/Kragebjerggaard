f8471005707945742368049541d8cab48772414a
cakao <stin915n@edu.zealand.dk>
2022-12-14 22:00:10 +0100

    api

---
+++ api.php
@@ -0,0 +1,29 @@
+<?php
+require "settings/init.php";
+
+$data = json_decode(file_get_contents('php://input'),true);
+
+header('Content-Type: application/json; charset=utf-8');
+
+if (isset($data["password"]) && $data["password"] == 'Krager') {
+    $sql = "SELECT * FROM produkter WHERE 1=1";
+    $bind = [];
+
+    if (!empty($data["nameSearch"])) {
+        $sql .= " AND (prodNavn LIKE CONCAT('%', :nameSearch, '%') OR prodKategori LIKE CONCAT('%', :nameSearch, '%') OR prodSort LIKE CONCAT('%', :nameSearch, '%')) ";
+        $bind[":nameSearch"] = $data["nameSearch"];
+
+    }
+
+    $boger = $db->sql($sql, $bind);
+    header("HTTP/1.1 200 OK");
+
+    echo json_encode($boger);
+
+} else {
+    header("HTTP/1.1 401 Unauthorized");
+    $error["errorMessage"] = "Dit kodeord var forkert";
+    echo json_encode($error);
+}
+
+?>
