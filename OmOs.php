fce717b072fa17829456166bd0ec8a951aafcde0
Oline <olinep@live.dk>
2022-12-15 16:13:47 +0100

    sat om os fil og kontakt fil op. Lige nu er der temp skrift i, skal rettes

---
+++ OmOs.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Chocolate Club</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<?php include "includes/navigation.php"; ?>
+
+<section class="card mx-auto" style="max-width: 50%;">
+    <img src=".." class="card-img-top" alt="personalet">
+    <div class="card-body">
+        <p class="card-text">Inde i køkkenet hos Chocolate Club, står der tre passionerede chokolatiers, der brænder for chokolade</p>
+        <br>
+        <p class="card-text">Ræveonkel Tobias Trads: der specialisere sig i sjove chokolader til børn og barnlige sjæle </p>
+        <br>
+        <p class="card-text">Kaninmor Amalie Mikkelsen: der laver eksklusiv chokolade til de mere kræsne</p>
+        <br>
+        <p class="card-text">Bamsefar Frederik Wolmer: Som har speciale i at lave chokolade til familiearrangementer</p>
+        <br>
+        <p class="card-text">Disse tre genier er gået sammen om at skabe en ekslusiv familie oplevelse, til dem der elsker chokolade</p>
+    </div>
+</section>
+
+<?php include "includes/footer.php"; ?>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
---
+++ contact.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Chocolate Club</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<?php include "includes/navigation.php"; ?>
+
+<section class="card" style="max-width: 100%;">
+    <div class="row g-0">
+        <div class="col-md-4 mt-5 mb-5">
+            <img src="..."  class="img-fluid rounded-start col-lg" alt="kontakt billede">
+        </div>
+        <div class="col-md-8 bg-grøn">
+            <div class="card-body">
+                <h3 class="card-title">Kontakt os</h3>
+                <p class="card-text">
+                    Hvis du har spørgsmål om:
+                </p>
+                <p class="card-text">
+                    vores forretning
+                </p>
+                <p class="card-text">
+                    produkter
+                </p>
+                <p class="card-text">
+                    arrangementer
+                </p>
+                <p class="card-text">
+                    eller rekruttering
+                </p>
+                <br>
+                <p class="card-text">
+                    Almindelige spørgsmål findes i vores <a href="FAQ">FAQ</a>
+                </p>
+                <p class="card-text">
+                    Hvis du har et yderligere spørgsmål
+                </p>
+                <p class="card-text">
+                    er du velkommen til at kontakte os
+                </p>
+                <p class="card-text">
+                    på: <a href="Info_dk@Chocolateclub.com">Info_dk@Chocolateclub.com</a>
+                </p>
+                <br>
+                <br>
+                <h3 class="card-title">Administration</h3>
+                <p class="card-text">
+                    Gadevej 30 DK-2860, Bagsværd
+                </p>
+                <p class="card-text">
+                    Tel 33 44 88 11
+                </p>
+                <p class="card-text">
+                    CVR 10 10 16 28
+                </p>
+                <br>
+                <p class="card-text">
+                    Fakturering til Chocolate Club:
+                </p>
+                <p class="card-text">
+                    <a href="Dksupplierinvoice@chocolateclub.se">Dksupplierinvoice@chocolateclub.se</a>
+                </p>
+                <p class="card-text pb-5">
+                    <a href="kontrolrapport">Kontrolrapport</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include "includes/footer.php"; ?>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
