5d3a26132bebfd670d00536484d6bb550ce2b78b
Oline <olinep@live.dk>
2022-12-15 18:55:48 +0100

    sat fontawesome linket ind i alle headers

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
 
-    <title>Chocolate Club</title>
+    <title>Kragebjerggaard</title>
 
     <meta name="robots" content="All">
     <meta name="author" content="Udgiver">
