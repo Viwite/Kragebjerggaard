d9fc7ba5b5f89e90ddf9e1edfefb8fbfdae4309c
Oline <olinep@live.dk>
2022-12-15 16:29:02 +0100

    sat folk og køer op der arbejder på gården på om os filen

--- OmOs.php
+++ OmOs.php
@@ -19,21 +19,82 @@
 
 <?php include "includes/navigation.php"; ?>
 
-<section class="card mx-auto" style="max-width: 50%;">
+<section class="card mx-auto my-5" style="max-width: 50%;">
     <img src=".." class="card-img-top" alt="personalet">
     <div class="card-body">
-        <p class="card-text">Inde i køkkenet hos Chocolate Club, står der tre passionerede chokolatiers, der brænder for chokolade</p>
+        <h1 class="card-text">Vores grundlægger, Lotte Kragh</h1>
         <br>
-        <p class="card-text">Ræveonkel Tobias Trads: der specialisere sig i sjove chokolader til børn og barnlige sjæle </p>
-        <br>
-        <p class="card-text">Kaninmor Amalie Mikkelsen: der laver eksklusiv chokolade til de mere kræsne</p>
-        <br>
-        <p class="card-text">Bamsefar Frederik Wolmer: Som har speciale i at lave chokolade til familiearrangementer</p>
-        <br>
-        <p class="card-text">Disse tre genier er gået sammen om at skabe en ekslusiv familie oplevelse, til dem der elsker chokolade</p>
+        <p class="card-text">Lotte Kragh er sjette generation af gårdejer på Kragebjerggård</p>
+        <p class="card-text">i 25 år har hun sørget for at gården er gået efter den grønne omstilling og nu kører efter bæredygtige premiser</p>
+        <p class="card-text">Hun sørger for dyrevelfærden på gården og går op i nulforbrug af sprøjtemidler</p>
     </div>
 </section>
 
+<section class="card mx-auto my-5" style="max-width: 50%;">
+    <img src=".." class="card-img-top" alt="personalet">
+    <div class="card-body">
+        <h1 class="card-text">Gårdman, Palle Kragh</h1>
+        <br>
+        <p class="card-text">Lotte Kragh er sjette generation af gårdejer på Kragebjerggård</p>
+        <p class="card-text">i 25 år har hun sørget for at gården er gået efter den grønne omstilling og nu kører efter bæredygtige premiser</p>
+        <p class="card-text">Hun sørger for dyrevelfærden på gården og går op i nulforbrug af sprøjtemidler</p>
+    </div>
+</section>
+
+<section class="card mx-auto my-5" style="max-width: 50%;">
+    <img src=".." class="card-img-top" alt="personalet">
+    <div class="card-body">
+        <h1 class="card-text">Vores landbrugsmedarbejder, Pia Kallesen</h1>
+        <br>
+        <p class="card-text">Lotte Kragh er sjette generation af gårdejer på Kragebjerggård</p>
+        <p class="card-text">i 25 år har hun sørget for at gården er gået efter den grønne omstilling og nu kører efter bæredygtige premiser</p>
+        <p class="card-text">Hun sørger for dyrevelfærden på gården og går op i nulforbrug af sprøjtemidler</p>
+    </div>
+</section>
+
+<section class="card mx-auto my-5" style="max-width: 50%;">
+    <img src=".." class="card-img-top" alt="personalet">
+    <div class="card-body">
+        <h1 class="card-text">Koen Ingeborg</h1>
+        <br>
+        <p class="card-text">Lotte Kragh er sjette generation af gårdejer på Kragebjerggård</p>
+        <p class="card-text">i 25 år har hun sørget for at gården er gået efter den grønne omstilling og nu kører efter bæredygtige premiser</p>
+        <p class="card-text">Hun sørger for dyrevelfærden på gården og går op i nulforbrug af sprøjtemidler</p>
+    </div>
+</section>
+
+<section class="card mx-auto my-5" style="max-width: 50%;">
+    <img src=".." class="card-img-top" alt="personalet">
+    <div class="card-body">
+        <h1 class="card-text">Ko fruen Gitte</h1>
+        <br>
+        <p class="card-text">Lotte Kragh er sjette generation af gårdejer på Kragebjerggård</p>
+        <p class="card-text">i 25 år har hun sørget for at gården er gået efter den grønne omstilling og nu kører efter bæredygtige premiser</p>
+        <p class="card-text">Hun sørger for dyrevelfærden på gården og går op i nulforbrug af sprøjtemidler</p>
+    </div>
+</section>
+
+<section class="card mx-auto my-5" style="max-width: 50%;">
+    <img src=".." class="card-img-top" alt="personalet">
+    <div class="card-body">
+        <h1 class="card-text"> Koen Dorte</h1>
+        <br>
+        <p class="card-text">Lotte Kragh er sjette generation af gårdejer på Kragebjerggård</p>
+        <p class="card-text">i 25 år har hun sørget for at gården er gået efter den grønne omstilling og nu kører efter bæredygtige premiser</p>
+        <p class="card-text">Hun sørger for dyrevelfærden på gården og går op i nulforbrug af sprøjtemidler</p>
+    </div>
+</section>
+
+<section class="card mx-auto my-5" style="max-width: 50%;">
+    <img src=".." class="card-img-top" alt="personalet">
+    <div class="card-body">
+        <h1 class="card-text">Gry</h1>
+        <br>
+        <p class="card-text">Lotte Kragh er sjette generation af gårdejer på Kragebjerggård</p>
+        <p class="card-text">i 25 år har hun sørget for at gården er gået efter den grønne omstilling og nu kører efter bæredygtige premiser</p>
+        <p class="card-text">Hun sørger for dyrevelfærden på gården og går op i nulforbrug af sprøjtemidler</p>
+    </div>
+</section>
 <?php include "includes/footer.php"; ?>
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
