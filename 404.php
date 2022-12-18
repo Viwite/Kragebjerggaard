9afc13bdbd16111e731bdbaebe3ee399baab0a98
Oline <olinep@live.dk>
2022-12-18 16:05:34 +0100

    uploadet 404 side og billede af sad cow

---
+++ 404.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Sigende titel</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<?php include "includes/navigation.php"; ?>
+
+<div class="container-fluid p-5">
+    <img src="uploads/"
+    <h1 class="text-1" style="text-align: center; font-family: lato, sans-serif; color: #1F897D;">ERROR</h1>
+    <h1 class="text-1" style="text-align: center; font-family: lato, sans-serif; color: #1F897D;">404:</h1>
+    <h1 class="text-1" style="text-align: center; font-family: lato, sans-serif; color: #1F897D;">THE COWS</h1>
+    <h1 class="text-1" style="text-align: center; font-family: lato, sans-serif; color: #1F897D;">RAN</h1>
+    <h1 class="text-1" style="text-align: center; font-family: lato, sans-serif; color: #1F897D;">AWAY!!</h1>
+    <h3 class="text-1" style="text-align: center; font-family: lato, sans-serif; color: #1F897D;">to get them back</h3>
+
+    <h3 class="text-1" style="text-align: center; font-family: lato, sans-serif; color: #1F897D;">please go back to the main page</h3>
+
+</div>
+
+<?php include "includes/footer.php"; ?>
+
+
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
